34b011d2b0f285f275ed021822d156dcdfd25962
DestroNic <32470624+DestroNic@users.noreply.github.com>
2019-12-01 20:57:21 -0800

    Portfolio base

---
+++ assets/navigation.js
@@ -0,0 +1,33 @@
+function homePage() {
+    let home = document.getElementById("container").innerHTML = `
+    <div class="background">
+            <div class="nav-menu">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Projects</a>
+                <a href="#">Contact</a>
+            </div>
+        <div class="logo-sign">
+            
+                <span class="top">83</span>
+            
+                <span class="bottom">dev</span>
+            
+        </div>
+        </div>
+    `;
+}
+
+function aboutPage() {
+    let about = document.getElementById("container").innerHTML = `
+    <div class="background2">
+            <div class="nav-menu">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Projects</a>
+                <a href="#">Contact</a>
+            </div>
+        
+        </div>
+    `;
+}
